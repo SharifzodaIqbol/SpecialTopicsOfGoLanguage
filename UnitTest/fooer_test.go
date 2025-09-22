68c726b9d49456289d6abd931650fb36f80ff630
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2025-09-22 11:42:37 +0300

    unittest

--- UnitTest/fooer_test.go
+++ UnitTest/fooer_test.go
@@ -1,10 +1,44 @@
 package unittest
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFooer(t *testing.T) {
 	result := Fooer(3)
 	if result != "Foo" {
 		t.Errorf("Result was incorrect, got: %s, want: %s", result, "Foo")
+	}
+}
+
+func TestFooer2(t *testing.T) {
+	input := 3
+	result := Fooer(3)
+	t.Logf("The input was %d", input)
+	if result != "Foo" {
+		t.Errorf("Result was incorrect, got: %s, was: %s", result, "Foo")
+	}
+	t.Fatalf("Stop the test now, we have seen enough")
+	t.Error("This won't be executed")
+}
+
+func TestFooerTableDriven(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input int
+		want  string
+	}{
+		{"9 should be Foo", 9, "Foo"},
+		{"3 should be Foo", 3, "Foo"},
+		{"1 is not Foo", 1, "1"},
+		{"0 should bee Foo", 0, "Foo"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ans := Fooer(tt.input)
+			if ans != tt.want {
+				t.Errorf("got: %s, want %s", ans, tt.want)
+			}
+		})
 	}
 }
